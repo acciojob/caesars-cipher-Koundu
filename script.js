c18f869399b01deb12af3ccadce62fed57b31da5
Koundu <121346044+Koundu@users.noreply.github.com>
2023-06-07 15:19:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,8 +13,15 @@ const lookup = {
 function rot13(encodedStr){
    let decodedArr = []; // Your Result goes here
   // Only change code below this line
-
-  return ;//return decodedArr
+	for(var i=0;i<encodedStr.length;i++){
+		var temp = encodedStr.charAt(i);
+		if(temp !== "" || temp!== "!" || temp !== "?"){
+			decodedArr += String.fromCharCode(13 + String.prototype.charCodeAt(temp));
+		}else{
+			encodedStr += temp;
+		}
+	}
+  return decodedArr;//return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
